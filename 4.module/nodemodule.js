dbc997119b3b366c36280565a097010ad426b6fd
leo8729 <298011431@qq.com>
2016-03-17 14:41:49 +0800

    201601node

--- 4.module/nodemodule.js
+++ 4.module/nodemodule.js
@@ -1,3 +1,10 @@
-/**
- * Created by 芄子酱 on 2016/3/16.
- */
+//file system
+//加载fs核心模块 node自带的模块
+var fs=require("fs");
+fs.readFile("./index.txt");
+
+
+//文件模块 就是我们自己写的模块文件模块
+require("./math.js");
+
+//别人给你的，第三者的，第三方模块
